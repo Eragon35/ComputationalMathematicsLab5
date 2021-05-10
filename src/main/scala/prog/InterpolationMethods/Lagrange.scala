bfdd08bc9fa82cdc9f82db7b96ab0119ee7ce114
Eragon35 <antipin35@yahoo.com>
2021-05-10 15:54:05 +0300

    Made Lagrange method

--- src/main/scala/prog/InterpolationMethods/Lagrange.scala
+++ src/main/scala/prog/InterpolationMethods/Lagrange.scala
@@ -1,5 +1,24 @@
 package prog.InterpolationMethods
 
 object Lagrange {
+  def solve(points: IndexedSeq[(Double, Double)], x: Double): Double = {
+    var answer = 0.0
+    points.foreach(f => answer += f._2 * (divider(x, f._1, points) / dividend(f._1, points)))
+    answer
+  }
+
+  def divider(X: Double, xi: Double, otherX: IndexedSeq[(Double, Double)]) : Double = {
+    var sum: Double = 1.0
+    for (x <- otherX.indices) if (x != xi) sum *= (X - x)
+//    print(s"$sum ")
+    sum
+  }
+
+  def dividend(xi: Double, otherX: IndexedSeq[(Double, Double)]) : Double = {
+    var sum: Double = 1.0
+    for (x <- otherX.indices) if (x != xi) sum *= (xi - x)
+//    print(s"$sum ")
+    sum
+  }
 
 }
