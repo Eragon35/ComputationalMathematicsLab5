747b1317bf6cf45ce640d21c9b58b0d8bfef858e
Eragon35 <antipin35@yahoo.com>
2021-05-10 18:55:35 +0300

    Fix Lagrange method

--- src/main/scala/prog/InterpolationMethods/Lagrange.scala
+++ src/main/scala/prog/InterpolationMethods/Lagrange.scala
@@ -9,16 +9,13 @@ object Lagrange {
 
   def divider(X: Double, xi: Double, otherX: IndexedSeq[(Double, Double)]) : Double = {
     var sum: Double = 1.0
-    for (x <- otherX.indices) if (x != xi) sum *= (X - x)
-//    print(s"$sum ")
+    for (x <- otherX) if (x._1 != xi) sum *= (X - x._1)
     sum
   }
 
   def dividend(xi: Double, otherX: IndexedSeq[(Double, Double)]) : Double = {
     var sum: Double = 1.0
-    for (x <- otherX.indices) if (x != xi) sum *= (xi - x)
-//    print(s"$sum ")
+    for (x <- otherX) if (x._1 != xi) sum *= (xi - x._1)
     sum
   }
-
 }
