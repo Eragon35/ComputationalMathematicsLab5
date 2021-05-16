9b5b7a1892a8b4abe3ca5d9164273aa38bb8ca22
Eragon35 <antipin35@yahoo.com>
2021-05-16 18:42:53 +0300

    Added tests

--- src/test/scala/Tests.scala
+++ src/test/scala/Tests.scala
@@ -1,10 +1,9 @@
 
-import junit.framework.{Assert, TestCase}
+import junit.framework.TestCase
 import junit.framework.TestCase.assertEquals
-import org.junit.Test
-import prog.InterpolationMethods.NewtonPolynomial.{solveRight, findDY}
+import prog.InterpolationMethods.NewtonPolynomial.{findDY, q2}
 
-class DeltaYTest extends TestCase{
+class Tests extends TestCase{
   private val data = IndexedSeq((0.1, 1.25), (0.2, 2.38), (0.3, 3.79), (0.4, 5.44), (0.5, 7.14))
   private val delta = 0.0000000000001
 
@@ -14,4 +13,9 @@ class DeltaYTest extends TestCase{
   def testPower0(): Unit = assertEquals(7.14,findDY(0, 4, data), delta)
   def testRofloTest(): Unit = assertEquals(0.19,"0.19".toDouble )
 //  def testNewton(): Unit = assertEquals(, solve(data, ))
+  def testQ0():Unit = assertEquals(-0.28, q2(0, 0.22, data.last._1), delta)
+  def testQ1():Unit = assertEquals(-0.2016, q2(1, 0.22, data.last._1), delta)
+  def testQ2():Unit = assertEquals(-0.346752, q2(2, 0.22, data.last._1), delta)
+  def testQ3():Unit = assertEquals(-0.94316544, q2(3, 0.22, data.last._1), delta)
+  def testQ4():Unit = assertEquals(-3.5085754368, q2(4, 0.22, data.last._1), delta)
 }
